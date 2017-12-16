54d36a633d359227fe6c716fbc27d9b9321749c6
marcio.soares <marcio@mail.com>
2017-12-16 18:45:42 -0200

    add total sell

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,6 +1,7 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
-  enum status: {finished: 0, canceled: 1}
+  
+  enum status: {finalizada: 0, cancelada: 1}
   belongs_to :discount
   belongs_to :client
   
@@ -9,14 +10,28 @@ class Sell < ApplicationRecord
   
   has_many :sell_services
   has_many :services, through: :sell_services
-
+  
   validates :client, presence: true
+
+  before_save :set_label
+
   def fae_display_field
     id
   end
   def self.for_fae_index
     order(:id)
   end
+  private
 
+    def set_label
+      total = 0
+      self.products.each{|p| total += p.price}
+      self.services.each{|s| total += s.price}
+      if self.discount.present?
+        total -= self.discount.value
+      end
+      total = total >= 0 ? total : 0
+      self.total = total
+    end
 
 end
