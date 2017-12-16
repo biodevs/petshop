26c0c8129fd5873299d69a92fb6cb5fdc950b24e
marcio.soares <marcio@mail.com>
2017-12-16 18:24:30 -0200

    many associations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,8 @@
 class Campaign < ApplicationRecord
   include Fae::BaseModelConcern
-
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients
+  
   validates :title, :body, presence: true
     
   def fae_display_field
